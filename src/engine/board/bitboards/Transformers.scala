d6f9e8fcfaa9f9517daa615c29b502b19955cebc
Melvic <melvic.ybanez2014@gmail.com>
2018-09-16 05:42:29 +0800

    Refactor transformers

--- src/engine/board/bitboards/Transformers.scala
+++ src/engine/board/bitboards/Transformers.scala
@@ -8,34 +8,30 @@ import Bitboard.U64
 object Transformers {
   type Transformation = U64 => U64
 
-  def horizontalMirror: Transformation = bitboard => {
-    val singleColMask = 0x5555555555555555L
-    val doubleColMask = 0x3333333333333333L
-    val quadColMask = 0x0f0f0f0f0f0f0f0fL
+  /**
+    * Shift even files to the left, odd files to the right, and combine the results.
+    * Do this recursively. In each iteration, double the column mask and steps.
+    */
+  def horizontalMirror: Transformation = flip(List(
+    0x5555555555555555L,
+    0x3333333333333333L,
+    0x0f0f0f0f0f0f0f0fL
+  ), 1)
 
-    // shift even files to the left, odd files to the right, and combine the results
-    var mirroredBoard = ((bitboard >> 1) & singleColMask) | ((bitboard & singleColMask) << 1)
+  /**
+    * Shift even ranks downward, odd ranks upward, and combine the result.
+    * Do this recursively. In each iteration, double the row mask and steps.
+    */
+  def verticalFlip: Transformation = flip(List(
+    0x00ff00ff00ff00ffL,
+    0x0000ffff0000ffffL
+  ), 8) andThen { flipped => (flipped >> 32) | (flipped << 32) }
 
-    // do the same thing with the previous result, but shift twice as much
-    mirroredBoard = ((mirroredBoard >> 2) & doubleColMask) | ((mirroredBoard & doubleColMask) << 2)
 
-    // do the same thing with the previous result, but shift twice as much (four times the original)
-    ((mirroredBoard >> 4) & quadColMask) | ((mirroredBoard & quadColMask) << 4)
-  }
-
-  def verticalFlip: Transformation = bitboard => {
-    val singleRowMask = 0x00ff00ff00ff00ffL
-    val doubleRowMask = 0x0000ffff0000ffffL
-
-    // shift even ranks downward, odd ranks upward, and combine the result
-    var flippedBoard = ((bitboard >> 8) & singleRowMask) | ((bitboard & singleRowMask) << 8)
-
-    // do the same thing with the previous result, but shift twice as much
-    flippedBoard = ((flippedBoard >> 16) & doubleRowMask) | ((flippedBoard & doubleRowMask) << 16)
-
-    // do the same thing with the previous result, but shift twice as much (four times the original)
-    (flippedBoard >> 32) | (flippedBoard << 32)
-  }
+  def flip(masks: List[U64], initialStep: Int): Transformation = bitboard =>
+    masks.foldLeft(bitboard, initialStep) { case ((bitset, step), mask) =>
+      (((bitset >> step) & mask) | ((bitset & mask) << step), step * 2)
+    }._1
 
   def rotate180: Transformation = horizontalMirror andThen verticalFlip
 }
