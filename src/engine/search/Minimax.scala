adc087f7348a1031badbe513d57234f024afafad
Melvic <melvic.ybanez2014@gmail.com>
2019-01-27 01:29:40 +0800

    Refactor Minimax

--- src/engine/search/Minimax.scala
+++ src/engine/search/Minimax.scala
@@ -1,49 +1,33 @@
 package engine.search
 
-import engine.board.{Board, Piece}
-import engine.movegen.Move.LocationMove
-
-import scala.annotation.tailrec
+import engine.board.Board
 
 /**
   * Created by melvic on 1/26/19.
   */
 sealed trait Minimax {
-  def init: Int
-  def betterScore(score1: Int, score2: Int): Boolean
-  def evaluate(board: Board): Int
+  def comparisonScore: Int => Int
+  def evaluate: Board => Int
 
-  def search(depth: Int, board: Board, max: Minimax, min: Minimax): (Board, Int) =
-    if (depth == 0) (board, evaluate(board))
-    else {
-      @tailrec
-      def recurse(board: Board, bestScore: Int, boards: Stream[(LocationMove, Piece)]): (Board, Int) = boards match {
-        case Seq() => (board, bestScore)
-        case (move, piece) +: xs =>
-          val movedBoard: Board = board.updateByMove(move, piece)
-          val (newBoard, score) = max.search(depth - 1, movedBoard, min, max)
+  def evaluate(depth: Int, board: Board): Int = {
+    def minimax(depth: Int, board: Board, max: Minimax, min: Minimax): Int =
+      if (depth == 0) max.evaluate(board)
+      else board.generateMoves.map { case (move, piece) =>
+        val movedBoard = board.updateByMove(move, piece)
+        val score = minimax(depth - 1, movedBoard, min, max)
+        comparisonScore(score)
+      }.max
 
-          // If the score is better than the current best, make it the new best.
-          // Otherwise, keep the current best score.
-          val (betterBoard, betterScore) =
-            if (betterScore(score, bestScore)) (newBoard, score)
-            else (board, bestScore)
-
-          recurse(betterBoard, betterScore, xs)
-      }
-
-      recurse(board, init, board.generateMoves)
-    }
+    minimax(depth, board, new Max, new Min)
+  }
 }
 
 class Max extends Minimax {
-  override val init = -Integer.MAX_VALUE
-  override def betterScore(score1: Int, score2: Int) = score1 > score2
-  override def evaluate(board: Board) = board.evaluate
+  override def evaluate = _.evaluate
+  override def comparisonScore = identity
 }
 
 class Min extends Minimax {
-  override val init = Integer.MAX_VALUE
-  override def betterScore(score1: Int, score2: Int) = score1 < score2
-  override def evaluate(board: Board) = -board.evaluate
+  override def evaluate = -_.evaluate
+  override def comparisonScore = -_
 }
