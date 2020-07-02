bc79d68a2ecc137c52326f2d24b6db07a4778a83
Melvic Ybanez <melvic@pop-os.localdomain>
2020-07-03 02:09:42 +0800

    Generalize grid coordinate computation

--- core/src/com/github/melvic/nostalgia/math/NCanvas.scala
+++ core/src/com/github/melvic/nostalgia/math/NCanvas.scala
@@ -3,16 +3,21 @@ package com.github.melvic.nostalgia.math
 import com.github.melvic.nostalgia.math.Coordinate.{GridCoordinate, CanvasCoordinate}
 
 trait NCanvas[C] {
+  type Offset = Bounds => Double
+
   def squareSize: Int
 
   def bounds: Bounds
 
   def offsettedCoordinate(
-      coordinate: CanvasCoordinate, offset: Bounds => Double
+      coordinate: CanvasCoordinate, offset: Offset
   ): coordinate.Type = coordinate.value + offset(bounds)
 
   def scaledCoordinate(coordinate: GridCoordinate, f: CanvasCoordinate => Double): Double =
     f(CanvasCoordinate(coordinate.value * squareSize))
+
+  def gridCoordinate(coordinate: CanvasCoordinate, offset: Offset) =
+    ((coordinate.value - offset(bounds) / squareSize)).toInt
 
   def x(cX: CanvasCoordinate): cX.Type = offsettedCoordinate(cX, _.west)
   def y(cY: CanvasCoordinate): cY.Type = offsettedCoordinate(cY, _.north)
@@ -20,8 +25,8 @@ trait NCanvas[C] {
   def x(gX: GridCoordinate): Double = scaledCoordinate(gX, x)
   def y(gY: GridCoordinate): Double = scaledCoordinate(gY, y)
 
-  def col(x: Double): Int = ((x - bounds.west) / squareSize).toInt
-  def row(y: Double): Int = ((y - bounds.east) / squareSize).toInt
+  def col(x: CanvasCoordinate): Int = gridCoordinate(x, _.west)
+  def row(y: CanvasCoordinate): Int = gridCoordinate(y, _.east)
 }
 
 object NCanvas {
