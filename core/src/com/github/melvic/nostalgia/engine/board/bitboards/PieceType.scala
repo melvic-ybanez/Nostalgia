b4c10336ec6fb1281583c5012796151ffae3fa5d
Melvic Ybanez <melvic@pop-os.localdomain>
2020-07-12 17:41:23 +0800

    Implement new bitboard piece type

---
+++ core/src/com/github/melvic/nostalgia/engine/board/bitboards/PieceType.scala
@@ -0,0 +1,10 @@
+package com.github.melvic.nostalgia.engine.board.bitboards
+
+object PieceType {
+  val Pawn = 0
+  val Knight = 1
+  val Bishop = 2
+  val Rook = 3
+  val Queen = 4
+  val King = 5
+}
