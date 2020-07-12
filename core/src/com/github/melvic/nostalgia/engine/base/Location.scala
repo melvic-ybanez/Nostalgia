b57d73336b44f657d66aabd25ad6351ad311fb48
Melvic Ybanez <melvic@pop-os.localdomain>
2020-07-12 21:13:01 +0800

    Update base move and location implementation

---
+++ core/src/com/github/melvic/nostalgia/engine/base/Location.scala
@@ -0,0 +1,3 @@
+package com.github.melvic.nostalgia.engine.base
+
+final case class Location[F, R](file: R, rank: R)
--- core/src/com/github/melvic/nostalgia/engine/base/Move.scala
+++ core/src/com/github/melvic/nostalgia/engine/base/Move.scala
@@ -2,4 +2,4 @@ package com.github.melvic.nostalgia.engine.base
 
 import scala.language.higherKinds
 
-final case class Move[P, M[_]](from: P, to: P, moveType: M[P])
+final case class Move[T, S, L](from: L, to: L, moveType: MoveType[Piece[T, S]])
